3ede1269aa0513e2124b66f9653c0c6e421c4a9d
egorovertskaya <mas_ska@list.ru>
2018-05-20 23:45:58 +0700

    blah-blah

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+Vue.component ("app-name",{
+	data: function() {
+		return {
+			table: [
+				{name: "Harry", points: 10, year: 2008},
+				{name: "Ron", points: 20, year: 2009},
+				{name: "Newill", points: 30, year: 2010},
+				{name: "Din", points: 40, year: 2011},
+				{name: "Sirius", points: 50, year: 1990},
+				{name: "Draco", points: 15, year: 2008},
+				{name: "Winsent", points: 25, year: 2009},
+				{name: "Gregory", points: 35, year: 2010},
+				{name: "Ewan", points: 45, year: 2011},
+				{name: "Orion", points: 55, year: 1990}
+			]
+		}
+	},
+	template: '<div><div class="name" v-for= "name in table"><p>{{ name.name }}</p></div></div>'
+}),
+
 new Vue({
-	el: '#vue-app',
+	el: "#app",
 	data: {
-		show: false,
-		// tables: [
-		// 	{name: "Harry", points: 10, year: 2008},
-		// 	{name: "Ron", points: 20, year: 2009},
-		// 	{name: "Newill", points: 30, year: 2010},
-		// 	{name: "Din", points: 40, year: 2011},
-		// 	{name: "Sirius", points: 50, year: 1990},
-		// 	{name: "Draco", points: 15, year: 2008},
-		// 	{name: "Winsent", points: 25, year: 2009},
-		// 	{name: "Gregory", points: 35, year: 2010},
-		// 	{name: "Ewan", points: 45, year: 2011},
-		// 	{name: "Orion", points: 55, year: 1990}
-		// 	]
+		show: true,
 	},
 });
